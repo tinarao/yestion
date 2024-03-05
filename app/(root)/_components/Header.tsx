b2e4f1a1be734cefcfd9038f7ba88d709b246b92
tinarao <golovindanila1613@gmail.com>
2024-03-05 20:35:33 +0300

    convex and clerk initialized

--- app/(root)/_components/Header.tsx
+++ app/(root)/_components/Header.tsx
@@ -4,9 +4,13 @@ import Logo from "@/components/Logo";
 import ThemeToggle from "@/components/theming/ThemeToggle";
 import { Button } from "@/components/ui/button";
 import { useScroll } from "@/hooks/useScroll";
+import { SignInButton, SignUpButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { LoaderIcon } from "lucide-react";
 
 const Header = () => {
   const scrolled = useScroll();
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
   return (
     <header
@@ -20,12 +24,23 @@ const Header = () => {
       <div className="flex space-x-10">
         <Logo size={30} />
       </div>
-      <div className="flex gap-16">
+      <div className="flex gap-16 items-center">
         <div>
           <ThemeToggle />
         </div>
-        <Button variant="ghost">Войти</Button>
-        <Button>Начать работу</Button>
+        {isLoading && <LoaderIcon className="animate-spin" />}
+        {!isAuthenticated && !isLoading && (
+          <div className="space-x-6">
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">Войти</Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button size="sm">
+                Нет аккаунта?
+              </Button>
+            </SignUpButton>
+          </div>
+        )}
       </div>
     </header>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theming/ThemeProvider";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { ConvexClientProvider } from "@/components/providers/ConvexProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,15 +19,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-background dark:bg-[#1F1F1F]`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="theme-yestion"
-        >
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="theme-yestion"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
