90896370a16414213ce3c8554d993ee39d9eece7
tinarao <golovindanila1613@gmail.com>
2024-03-05 21:01:25 +0300

    some conditional button rendering

--- app/(root)/_components/Header.tsx
+++ app/(root)/_components/Header.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import Loader from "@/components/Loader";
 import Logo from "@/components/Logo";
 import ThemeToggle from "@/components/theming/ThemeToggle";
 import { Button } from "@/components/ui/button";
 import { useScroll } from "@/hooks/useScroll";
-import { SignInButton, SignUpButton } from "@clerk/clerk-react";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
 import { useConvexAuth } from "convex/react";
 import { LoaderIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   const scrolled = useScroll();
@@ -24,23 +26,31 @@ const Header = () => {
       <div className="flex space-x-10">
         <Logo size={30} />
       </div>
-      <div className="flex gap-16 items-center">
-        <div>
-          <ThemeToggle />
-        </div>
-        {isLoading && <LoaderIcon className="animate-spin" />}
+      <div className="flex gap-6 items-center">
+        {isLoading && <Loader size="lg" />}
         {!isAuthenticated && !isLoading && (
           <div className="space-x-6">
             <SignInButton mode="modal">
-              <Button variant="ghost" size="sm">Войти</Button>
+              <Button variant="ghost" size="sm">
+                Войти
+              </Button>
             </SignInButton>
             <SignUpButton mode="modal">
-              <Button size="sm">
-                Нет аккаунта?
-              </Button>
+              <Button size="sm">Нет аккаунта?</Button>
             </SignUpButton>
           </div>
         )}
+        {isAuthenticated && !isLoading && (
+          <div className="flex gap-4">
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Приступить к работе</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        )}
+        <div>
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
--- app/(root)/_components/Hero.tsx
+++ app/(root)/_components/Hero.tsx
@@ -4,40 +4,58 @@ import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 
-import HeroDocsDark from "@/public/hero/home-hero-dark.png"
-import HeroDocs from "@/public/hero/home-hero.webp"
+import HeroDocsDark from "@/public/hero/home-hero-dark.png";
+import HeroDocs from "@/public/hero/home-hero.webp";
+import { useConvexAuth } from "convex/react";
+import Loader from "@/components/Loader";
+import { useTheme } from "next-themes";
+import Link from "next/link";
 
 const Hero = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const { theme, setTheme } = useTheme();
+
   return (
     <section className="w-full space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Пишите. Планируйте. Делитесь.<br /><span>А мы вам поможем</span>
+        Пишите. Планируйте. Делитесь.
+        <br />
+        <span>А мы вам поможем</span>
       </h1>
       <h3 className="text-md sm:text-xl md:text-2xl font-medium">
         <span className="underline">Yestion</span> - почти как Notion, правда,
         не совсем
       </h3>
-      <div className="flex flex-col md:flex-row gap-4 lg:gap-8 text-lg w-fit mx-auto">
-        <Button>
-          Приступить к работе
-          <ArrowRight className="h-4 ml-2" />
-        </Button>
+      <div className="flex flex-col w-fit mx-auto py-4">
+        {isLoading ? (
+          <Loader size="xl" />
+        ) : (
+          <Button asChild>
+            <Link href="/documents" className="flex items-center">
+              Приступить к работе
+              <ArrowRight className="h-4 ml-2" />
+            </Link>
+          </Button>
+        )}
       </div>
       <div className="w-fit mx-auto">
-        <Image 
-          src={HeroDocs} 
-          height={300} 
-          width={600} 
-          alt="Drawings"
-          className="dark:hidden pointer-events-none"
-        />
-        <Image 
-          src={HeroDocsDark} 
-          height={300} 
-          width={600} 
-          alt="Drawings"
-          className="light:hidden pointer-events-none"
-        />
+        {theme === "light" ? (
+          <Image
+            src={HeroDocs}
+            height={300}
+            width={600}
+            alt="Drawings"
+            className="dark:hidden pointer-events-none"
+          />
+        ) : (
+          <Image
+            src={HeroDocsDark}
+            height={300}
+            width={600}
+            alt="Drawings"
+            className="light:hidden pointer-events-none"
+          />
+        )}
       </div>
     </section>
   );
